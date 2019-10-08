f224d39cece68e254f05b4c0155c3416d402854a
YumiYukiko <eka112412@gmail.com>
2019-10-09 00:35:39 +0300

    первая партия

--- HW1/task8.py
+++ HW1/task8.py
@@ -1,22 +1,28 @@
-# Задание 8. Уровень - жестоко.
+import string
+ALLbukovi = set()
+bukovi = dict()
 
-# Условие: напишите программу, которая для данного текста подсчитывает частоты всех символов в нем. 
-# Регистр не важен. То есть символы 'А' и 'а' - эквивалентны.
+s = input().lower()
 
+for c in s:
+    if c in string.punctuation or c == ' ':
+        pass
+    elif not(c in ALLbukovi):
+        ALLbukovi.add(c)
+        bukovi.update({c:1})
+    else:
+        bukovi[c] += 1
 
+while bukovi:
+    mas = list()
+    val = max(bukovi.values())
+    for key in bukovi.keys():
+        if bukovi[key] == val:
+            mas.append(key)
+    
+    for key in mas: 
+        bukovi.pop(key)
 
-# Входные данные: произвольный текст.
-
-# Выходные данные: таблица с частотами символов. 
-# Таблица должна быть отсортирована по убыванию частот, в случае равных частот — в алфавитном порядке. 
-# Если в тексте нет алфавитных символов - выводим перенос строки.
-
-# Пример:
-# Ввод:                                      # Вывод:
-# hello, world!                              l: 3
-#                                            o: 2
-#                                            d: 1
-#                                            e: 1
-#                                            h: 1
-#                                            r: 1
-#                                            w: 1
+    mas.sort()
+    for i in mas:
+        print(str(i) + ': ' + str(val))
